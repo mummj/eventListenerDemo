65293ecdb9704a16a8796c3f72a59103e8f4fc9d
mummj <mummj@eou.edu>
2022-03-29 09:21:41 -0700

    finished demo

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 'use strict'
 
 const title = document.createElement('h1');
+const demo = document.getElementById('Demo');
 title.textContent='Event Listern Demo';
-document.body.appendChild(title);
+demo.appendChild(title);
 
-document.getElementById('myButton').addEventListener("click", function(){
-  alert('button was clicked');
-})
+function buttonClick(){
+ let buttonText = document.createElement('h1');
+ let textArea = document.getElementById('buttonInfo');
+ buttonText.textContent = "Button Was Clicked";
+ textArea.appendChild(buttonText);
+
+}
+
+document.getElementById('myButton').addEventListener("click", buttonClick);
+
+// document.getElementById('myButton').addEventListener("click", function(){
+//   alert('button was clicked');
+// })
